53c50811f9517b8e2e4f57c950323b554e991029
Sandeep Kumar Mahto <blackdemon875@gmail.com>
2021-11-26 13:30:50 +0530

    First commit

---
+++ config.js
@@ -0,0 +1,46 @@
+let colorsSizes = {
+    '2': {
+      size: 64,
+      color: '#f8e7ee'
+    },
+    '4': {
+      size: 64,
+      color: '#fff6ce'
+    },
+    '8': {
+      size: 64,
+      color: '#c9e9ec'
+    },
+    '16': {
+      size: 64,
+      color: '#f18892'
+    },
+    '32': {
+      size: 64,
+      color: '#f3bd19'
+    },
+    '64': {
+      size: 64,
+      color: '#ea2128'
+    },
+    '128': {
+      size: 52,
+      color: '#53d06a'
+    },
+    '256': {
+      size: 52,
+      color: '#0000ff'
+    },
+    '512': {
+      size: 52,
+      color: '#ff3e96'
+    },
+    '1024': {
+      size: 40,
+      color: '#3bdd6d'
+    },
+    '2048': {
+      size: 40,
+      color: '#ff0000'
+    }
+  };
